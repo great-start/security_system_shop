7745cac87c23834f8483b8ea73b0764f979ab0f8
Evgen <zhenya.bill@gmail.com>
2022-08-23 10:41:54 +0300

    22.08.22 - personalData

--- client/src/pages/UserData/UserData.tsx
+++ client/src/pages/UserData/UserData.tsx
@@ -1,11 +1,13 @@
-import React, { FC, useEffect } from 'react';
-import { Container, ListGroup, Spinner } from 'react-bootstrap';
+import React, { FC, useEffect, useState } from 'react';
+import { Button, Container, ListGroup, Spinner } from 'react-bootstrap';
 
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { getPersonalDataAsync } from '../../store';
 import css from './UserData.module.css';
 
 export const UserData: FC = () => {
+
+  const [personalDataChange, setPersonalDataChange] = useState(false);
   const dispatch = useAppDispatch();
   const { personalData, isLoading } = useAppSelector(state => state.personalDataReducer);
 
@@ -15,27 +17,40 @@ export const UserData: FC = () => {
 
   console.log(personalData);
 
+  const changePersonalData = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setPersonalDataChange(true);
+
+  }
+
   return ( !isLoading ?
-    <div className={css.userData}>
-      <div>
-        <ListGroup variant='flush' className={css.userDataFields}>
-          <ListGroup.Item variant='light'>
-            <span>Ім'я: </span>
-            {personalData?.firstName}</ListGroup.Item>
-          <ListGroup.Item variant='light'>
-            <span>Прізвище: </span>
-            {personalData?.lastName}</ListGroup.Item>
-          <ListGroup.Item variant='light'>
-            <span>Пошта: </span>
-            {personalData?.email}</ListGroup.Item>
-          <ListGroup.Item variant='light'>
-            <span>Телефон: </span>
-          </ListGroup.Item>
-        </ListGroup>
+    <div>
+      <div className={css.userData}>
+        <div>
+          <ListGroup variant='flush' className={css.userDataFields}>
+            <ListGroup.Item variant='light'>
+              <span>Ім'я: </span>
+              {!personalDataChange && personalData?.firstName}</ListGroup.Item>
+            <ListGroup.Item variant='light'>
+              <span>Прізвище: </span>
+              {!personalDataChange && personalData?.lastName}</ListGroup.Item>
+            <ListGroup.Item variant='light'>
+              <span>Пошта: </span>
+              {personalData?.email}</ListGroup.Item>
+            <ListGroup.Item variant='light'>
+              <span>Телефон: </span>
+            </ListGroup.Item>
+          </ListGroup>
+        </div>
+        <div>
+          <div>Дата створення акаунту:</div>
+          <p>{personalData?.createdAt}</p>
+        </div>
       </div>
-      <div>
-        <div>Дата створення акаунту: </div>
-        <p>{personalData?.createdAt}</p>
+      <div className={css.personalDataChange}>
+        <Button variant={'outline-success'} onClick={(e) => changePersonalData(e)}>
+          Змінити персональні дані
+        </Button>
       </div>
     </div>
    : <Spinner animation={'grow'} variant="primary" />);
