2287d14fee162f47be41dc02ca488e416f4e63b1
Evgen <zhenya.bill@gmail.com>
2022-08-23 12:22:01 +0300

    22.08.22 - personalData

--- server/src/user/dto/update-user.dto.ts
+++ server/src/user/dto/update-user.dto.ts
@@ -1,8 +1,7 @@
-import { SignUpUserDto } from '../../auth/dto/signUp.user.dto';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
-export class UpdateUserDto extends SignUpUserDto {
+export class UpdateUserDto {
   @ApiProperty({ example: 'Vanya', description: 'user firstName' })
   @IsString()
   @IsNotEmpty()
