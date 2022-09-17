799942d387ab8b70695564c52812292c380cdd77
Evgen <zhenya.bill@gmail.com>
2022-09-17 13:05:23 +0300

    clean code

--- client/src/components/PersonalPage/PersonalPage.tsx
+++ client/src/components/PersonalPage/PersonalPage.tsx
@@ -20,8 +20,6 @@ export const PersonalPage: FC = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
-  console.log(isAuth, isLoading, isAdmin);
-
   useEffect(() => {
     const path = pathname.split('/')[2] || null;
     if (path && Object.keys(protectedPages).includes(path)) {
