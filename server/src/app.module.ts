a77e5c28fb1e407f4184ae18fca85ed584c3d7e1
Evgen <zhenya.bill@gmail.com>
2022-10-09 16:17:18 +0300

    some changes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -14,7 +14,7 @@ import { AdminModule } from './admin/admin.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env.example',
+      envFilePath: '.env',
       isGlobal: true,
     }),
     ProductModule,
