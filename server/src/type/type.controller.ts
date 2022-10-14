46527b0c17591ccb0bbf21007cddb243c041d8f3
Evgen <zhenya.bill@gmail.com>
2022-10-14 20:50:55 +0300

    add new functionality (add type)

--- server/src/type/type.controller.ts
+++ server/src/type/type.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { TypeService } from './type.service';
 import { CreateTypeDto } from './dto/create-type.dto';
 import { UpdateTypeDto } from './dto/update-type.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtCheckGuard } from '../auth/guards/jwt-check.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/models/roles.enum';
 
 @ApiTags('type')
 @Controller('type')
 export class TypeController {
   constructor(private readonly typeService: TypeService) {}
 
-  @Post()
+  @UseGuards(JwtCheckGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Post('/')
   create(@Body() type: CreateTypeDto) {
     return this.typeService.create(type);
   }
