5b2d1d79227a099f35585d4b594e80a6e5727aa1
Evgen <zhenya.bill@gmail.com>
2022-10-14 20:52:52 +0300

    add new functionality (add type)

--- server/src/type/type.module.ts
+++ server/src/type/type.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+
 import { TypeService } from './type.service';
 import { TypeController } from './type.controller';
 import { PrismaService } from '../prisma.service';
+import { JwtCheckGuard } from '../auth/guards/jwt-check.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { AuthService } from '../auth/auth.service';
+import { UserService } from '../user/user.service';
+import { TokenService } from '../auth/token.service';
 
 @Module({
   controllers: [TypeController],
-  providers: [TypeService, PrismaService],
+  providers: [
+    TypeService,
+    PrismaService,
+    JwtCheckGuard,
+    RolesGuard,
+    AuthService,
+    UserService,
+    TokenService,
+  ],
 })
 export class TypeModule {}
